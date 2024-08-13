b4ddd1288c080f2d74f68c9e73b4355b8bb1ef46
GeorgesZam <154804622+GeorgesZam@users.noreply.github.com>
2024-08-13 22:56:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 joueurs = ["Georges", "Cécile", "Isabelle", "Alix"]
 
 # Liste des rôles disponibles (1 rôle par joueur)
-roles_disponibles = ["Loup Garou", "Villageois", "Voyante", "Villageois"]
+roles_disponibles = ["Loup Garou", "Sorcière", "Voyante", "Villageois"]
 
 # Attribution des rôles de manière aléatoire
 if 'roles' not in st.session_state:
@@ -68,7 +68,8 @@ def phase_loup_garou():
             key=f"victime_select_{st.session_state.phase}"  # Utilisation d'une clé unique
         )
         if st.button("Confirmer la victime", key=f"confirmer_victime_{st.session_state.phase}"):
-            st.write(f"{victime} a été tué(e). Son rôle était {st.session_state.roles[victime]}.")
+            st.session_state.victime_morte = f"{victime} a été tué(e). Son rôle était {st.session_state.roles[victime]}."
+            st.write(f"**{st.session_state.victime_morte}**")
             say(f"{victime} a été tué(e). Son rôle était {st.session_state.roles[victime]}.")
             del st.session_state.roles[victime]  # Supprime le joueur mort
             st.session_state.loup_garou_done = True
@@ -85,7 +86,8 @@ def phase_villageois():
     st.header("Phase du Vote des Villageois")
     vote = st.selectbox("Villageois, choisissez un joueur à éliminer:", [j for j in st.session_state.roles.keys()], key=f"vote_village_{st.session_state.phase}")
     if st.button("Confirmer l'élimination", key=f"eliminer_vote_{st.session_state.phase}"):
-        st.write(f"{vote} a été éliminé(e). Son rôle était {st.session_state.roles[vote]}.")
+        st.session_state.victime_votee = f"{vote} a été éliminé(e). Son rôle était {st.session_state.roles[vote]}."
+        st.write(f"**{st.session_state.victime_votee}**")
         say(f"{vote} a été éliminé(e). Son rôle était {st.session_state.roles[vote]}.")
         del st.session_state.roles[vote]  # Supprime le joueur éliminé
         st.session_state.villageois_done = True
@@ -114,6 +116,13 @@ phases = [phase_voyante, phase_loup_garou, phase_villageois]
 
 # Affichage des rôles avant chaque phase
 afficher_roles()
+
+# Affichage permanent des victimes
+if 'victime_morte' in st.session_state:
+    st.write(f"**{st.session_state.victime_morte}**")
+
+if 'victime_votee' in st.session_state:
+    st.write(f"**{st.session_state.victime_votee}**")
 
 if not verifier_fin_partie():
     phases[st.session_state.phase]()
